49b56e0ccbca20be5ac6fbfa2b1fde0741786f9a
richswag009 <richesmetelewawon@gmail.com>
2022-04-26 00:08:28 +0100

    made changes to javascript

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-document.querySelector('.search button').addEventListener('click', ()=>{
-    weather.search()
-  })
-document.querySelector('.search-bar').addEventListener('keyup', (event)=>{
-    if(event.key=="Enter"){weather.search()}
-  })
-  
+const buttons =document.querySelector(".search button")
+const SearchBar=document.querySelector('.search-bar');
+const Weathercity =document.querySelector('.city');
+const weathericon =document.querySelector('.icon');
+const weatherdescription =document.querySelector('.description');
+const weatherhumidity=document.querySelector('.humidity');
+const weatherTemp = document.querySelector('.temp');
+const weatherWind =document.querySelector('.wind');
+const mainWeather = document.querySelector('.Weather');
+
+buttons.addEventListener('click', () => {
+        weather.search();
+    })
+    
+SearchBar.addEventListener('keyup', (event)=>{
+    if(event.key=="Enter"){
+        weather.search()
+    }
+  });
+
 let weather = {
     apikey:"69bbeb705b15b037e961d93295031e73",
+
     fetchWeather: function(city) {
         fetch(
             "https://api.openweathermap.org/data/2.5/weather?q="
@@ -17,23 +31,25 @@ let weather = {
             .then((response) => response.json())
             .then((data) => this.displayWeather(data));
     },
+
     displayWeather: function(data){
         const {name}=data;
         const {icon, description}=data.weather[0];
         const {temp, humidity}=data.main;
         const{speed}= data.wind;
-        document.querySelector('.city').innerText="Weather in "+name;
-        document.querySelector('.icon').src="https://openweathermap.org/img/wn/"+ icon +".png"
-        document.querySelector('.description').innerText=description;
-        document.querySelector('.humidity').innerText=`humidity: ${humidity}%`;
-        document.querySelector('.temp').innerText=` ${temp} °F`;
-        document.querySelector('.wind').innerText= `wind speed : ${speed}km/hr`;
-        document.querySelector('.Weather').classList.remove("loading");
+        Weathercity.innerText="Weather in "+name;
+        weathericon.src="https://openweathermap.org/img/wn/"+ icon +".png"
+        weatherdescription.innerText=description;
+        weatherhumidity.innerText=`humidity: ${humidity}%`;
+        weatherTemp.innerText=` ${temp} °F`;
+        weatherWind.innerText= `wind speed : ${speed}km/hr`;
+        mainWeather.classList.remove("loading");
         document.body.style.backgroundImage="url('https://source.unsplash.com/1600x900/?"+description+"')"
-    },
+},
     search: function(){
-        this.fetchWeather(document.querySelector('.search-bar').value);
+        this.fetchWeather(SearchBar.value);
 
     }
 };
 weather.fetchWeather("afikpo");
+
