909e6feb7f75c68e2bac454e0dfe946928956f53
richswag009 <richesmetelewawon@gmail.com>
2022-04-25 09:51:30 +0100

    updating js file

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ document.querySelector('.search button').addEventListener('click', ()=>{
 document.querySelector('.search-bar').addEventListener('keyup', (event)=>{
     if(event.key=="Enter"){weather.search()}
   })
+  
 let weather = {
     apikey:"69bbeb705b15b037e961d93295031e73",
     fetchWeather: function(city) {
@@ -23,13 +24,13 @@ let weather = {
         const{speed}= data.wind;
         document.querySelector('.city').innerText="Weather in "+name;
         document.querySelector('.icon').src="https://openweathermap.org/img/wn/"+ icon +".png"
-    document.querySelector('.description').innerText=description;
-    document.querySelector('.humidity').innerText=`humidity: ${humidity}%`;
-    document.querySelector('.temp').innerText=` ${temp} °F`;
-    document.querySelector('.wind').innerText= `wind speed : ${speed}km/hr`;
-    document.querySelector('.Weather').classList.remove("loading");
-    document.body.style.backgroundImage="url('https://source.unsplash.com/1600x900/?"+description+"')"
-},
+        document.querySelector('.description').innerText=description;
+        document.querySelector('.humidity').innerText=`humidity: ${humidity}%`;
+        document.querySelector('.temp').innerText=` ${temp} °F`;
+        document.querySelector('.wind').innerText= `wind speed : ${speed}km/hr`;
+        document.querySelector('.Weather').classList.remove("loading");
+        document.body.style.backgroundImage="url('https://source.unsplash.com/1600x900/?"+description+"')"
+    },
     search: function(){
         this.fetchWeather(document.querySelector('.search-bar').value);
 
